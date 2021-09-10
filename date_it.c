1746bc17028eab5b35ebe16131e06b83dc55ede6
Dylan Leifer-Ives <40322629+dleiferives@users.noreply.github.com>
2021-09-09 23:25:09 -0700

    Did a code face lift

--- date_it.c
+++ date_it.c
@@ -2,26 +2,31 @@
 #include <time.h>
 #include <windows.h>
 #include <ctype.h>
+/* lazy definitions to remove magic numbers... which are used anyways in some cases */
 #define SEP 0xBD
 #define COLON 0xBA
 
+long long times_printed =0;
+
+/* Virtually types given character and wether it is a press or a realease */
 void type_char(unsigned int val, DWORD type)
 {
-		INPUT ip;
-// ...
-    // Set up a generic keyboard event.
-    ip.type = INPUT_KEYBOARD;
-    ip.ki.wScan = 0; // hardware scan code for key
-    ip.ki.time = 0;
-    ip.ki.dwExtraInfo = 0;
+		/* create a virual-keyboard event */
+		INPUT input;
+    input.type = INPUT_KEYBOARD;
+    input.ki.wScan = 0;
+    input.ki.time = 0;
+    input.ki.dwExtraInfo = 0;
 
-    ip.ki.wVk = val; // virtual-key code for the "a" key
-    ip.ki.dwFlags = type; // 0 for key press
-    SendInput(1, &ip, sizeof(INPUT));
+    input.ki.wVk = val; // virtual-key code for the val 
+    input.ki.dwFlags = type; // 0 for key press
+    SendInput(1, &input, sizeof(INPUT));
 }
 
+/* Uses type_char to print out a string */
 void type_string(char * buffer)
 {
+	/* iterates through passed string and writes all chars */
 	int iterator =0;
 	while(buffer[iterator] != '\0')
 	{
@@ -31,8 +36,10 @@ void type_string(char * buffer)
 	}
 }
 
+/* a hacky implementation to type the colons */
 void type_colon(void)
 {
+	/* Lifts up control, then presses shift semicolon and releases shift */
 	type_char(0x11,KEYEVENTF_KEYUP);
 	type_char(0x10,0);
 	type_char(COLON,0);
@@ -40,47 +47,54 @@ void type_colon(void)
 }
 
 
+/* the core function. quite literally types out the date */
 int print_date(int detail)
 {
 
+	/* unpresses shift control and alt (in order here as is below) */
 	type_char(0x10,KEYEVENTF_KEYUP);
 	type_char(0x11,KEYEVENTF_KEYUP);
 	type_char(0x12,KEYEVENTF_KEYUP);
 
-	printf("hello");
+	printf("Time Printed %i times \r",times_printed);
+	/* accessing the time from the computer */
 	SYSTEMTIME t;
-
 	GetLocalTime(&t);
+	/* creating the buffer where each value will be stored */
 	char buffer[100];
-	/* t.wYear, t.wMonth, t.wDay, t.wHour, */ 
-	/* t.wMinute, t.wSecond, t.wMilliseconds */
 
-
+	/* only called if the ctrl+k has been pressed, does the date */
 	if(detail > -1)
 	{
-
+		/* types the date */
+		/* types the month */
 		sprintf(buffer,"%d", t.wMonth);  
 		type_string(buffer);
 
 
 		type_char(SEP,0);
 
+		/* types the day */
 		sprintf(buffer,"%d", t.wDay);  
 		type_string(buffer);
 
 		type_char(SEP,0);
 
 
+		/* types the year */
 		sprintf(buffer,"%d", t.wYear);  
 		type_string(buffer);
 
 
 
+
+		/* enable hours minutes and seconds */ 
+		if(detail > 0)
+		{
+		/* presses space when needed */
 		type_char(VK_SPACE,0);
 		type_char(VK_SPACE,KEYEVENTF_KEYUP);
 
-		if(detail > 0)
-		{
 			sprintf(buffer,"%d", t.wHour);  
 			type_string(buffer);
 
@@ -94,6 +108,7 @@ int print_date(int detail)
 			sprintf(buffer,"%d", t.wSecond);
 			type_string(buffer);
 
+			/* enable miliseconds */ 
 			if(detail >1)
 			{
 
@@ -101,55 +116,44 @@ int print_date(int detail)
 
 				sprintf(buffer,"%d", t.wMilliseconds);  
 				type_string(buffer);
-				type_char(0x12,0);
-
+				type_char(0x12,0); // If alt was pressed repress it
 			}
-			type_char(0x10,0);
+			type_char(0x10,0);// If shift was pressed repress it
 		}
-	}
 	type_char(VK_SPACE,0);
 	type_char(VK_SPACE,KEYEVENTF_KEYUP);
-
-
-
-	/* 	type_char(COLON); */
-
-
-
-	type_char(0x11,0);
-
-
-	puts(buffer);
+	type_char(0x11,0); // If ctrl was presed... represses it
+	times_printed++;
+	}
 }
 
 
 
 
-/* int get_key_code */
 int main(void)
 {
-
-	/* BYTE b[256]; */
-	/* BYTE o[256]; // original keyboardstate */
-
-
+	// Main loop
 	while(1)
 	{
+		/* read in the current key values that are pressed down */
 		int v0 = GetKeyState(VK_CONTROL) &8000;
 		int v1 = GetKeyState(VK_SHIFT) &8000;
 		int v2 = GetKeyState(VK_MENU) &8000;
 		int v3 = GetKeyState('K') &8000;
 
+		/* test if ctrl + k are pressed but shift and alt are not */
 		if(v0  && v3 && (!v1) && (!v2))
 		{
 			print_date(0);
 			Sleep(100);
 		}
+		/* test if ctrl + shift + k are pressed but alt is are not */
 		if(v0 && v3 && v1 && (!v2))
 		{
 			print_date(1);
 			Sleep(100);
 		}
+		/* test if ctrl + shift + k + alt are pressed */ 
 		if(v0 && v3 && v1 && v2)
 		{
 			print_date(10);
